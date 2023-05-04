cdaf829fd7225e8c741de11162790d9884445a0d
ai-fn-upd <132555753+ai-fn-upd@users.noreply.github.com>
2023-05-04 12:48:23 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+base_python_interpreter=""
+project_domain=""
+project_path=`pwd`
+
+read -p "Python interpreter: " base_python_interpreter
+read -p "Your domain without protocol (for example, google.com): " project_domain
+`$base_python_interpreter -m venv env`
+source env/bin/activate
+pip install -U pip
+pip install -r requirements.txt
